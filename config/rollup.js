2be4ec97fcc9e397155e1c5415d929a9b75f9bbd
颜海镜 <yanxuefeng@meituan.com>
2018-03-18 12:17:50 +0800

    feat: banner信息改为手动配置

--- config/rollup.js
+++ config/rollup.js
@@ -4,15 +4,12 @@ var pkg = require('../package.json');
 var name = pkg.name.split('/').pop();
 var version = pkg.version;
 
-// 兼容 "author": "yanhaijing" 和 "author": {"name": "yanhaijing"}
-var author = pkg.author.name || pkg.author;
-
 var banner = 
 `/*!
- * ${name} ${version} (https://github.com/${author}/${name})
- * API https://github.com/${author}/${name}/blob/master/doc/api.md
- * Copyright 2017-${(new Date).getFullYear()} ${author}. All Rights Reserved
- * Licensed under MIT (https://github.com/${author}/${name}/blob/master/LICENSE)
+ * jslib-base ${version} (https://github.com/yanhaijing/jslib-base)
+ * API https://github.com/yanhaijing/jslib-base/blob/master/doc/api.md
+ * Copyright 2017-${(new Date).getFullYear()} yanhaijing. All Rights Reserved
+ * Licensed under MIT (https://github.com/yanhaijing/jslib-base/blob/master/LICENSE)
  */
 `;
 
