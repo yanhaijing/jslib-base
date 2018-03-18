381df4b3bf7ee88fb0bde243ff0fdc910e80e3da
颜海镜 <yanxuefeng@meituan.com>
2018-03-18 11:47:07 +0800

    feat: 添加banner功能，简化配置
    1. 添加banner功能
    2. 仓库名称读取package.json

--- config/rollup.config.aio.js
+++ config/rollup.config.aio.js
@@ -4,13 +4,16 @@ var babel = require("rollup-plugin-babel");
 var nodeResolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
 
+var common = require('./rollup.js');
+
 export default {
     input: 'src/index.js',
     output: {
         file: 'dist/index.aio.js',
         format: 'umd',
-        name: 'jslib_base'
+        name: common.name
     },
+    banner: common.banner,
     plugins: [
         nodeResolve({
             main: true
--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 
-var babel = require("rollup-plugin-babel");
+var babel = require('rollup-plugin-babel');
+var common = require('./rollup.js');
 
 export default {
     input: 'src/index.js',
@@ -8,6 +9,7 @@ export default {
         file: 'dist/index.js',
         format: 'cjs',
     },
+    banner: common.banner,
     plugins: [
         babel({
             exclude: 'node_modules/**'
