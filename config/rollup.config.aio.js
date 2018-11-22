dd7ac7485fdc6170a881052c92be0269428b4596
yugasun <yuga_sun@163.com>
2018-11-22 10:01:49 +0800

    feat: add uglify plugin

--- config/rollup.config.aio.js
+++ config/rollup.config.aio.js
@@ -1,15 +1,18 @@
 // rollup.config.js
 
-var babel = require("rollup-plugin-babel");
+var babel = require('rollup-plugin-babel');
 var nodeResolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
+var uglify = require('rollup-plugin-uglify').uglify;
 
 var common = require('./rollup.js');
 
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/index.aio.js',
+        file: process.env.NODE_ENV === 'production' 
+            ? 'dist/index.aio.min.js' 
+            : 'dist/index.aio.js',
         format: 'umd',
         // 如果不同时使用 export 与 export default 可打开legacy
         // legacy: true,
@@ -26,6 +29,7 @@ export default {
         babel({
             runtimeHelpers: true,
             exclude: 'node_modules/**'
-        })
+        }),
+        (process.env.NODE_ENV === 'production' && uglify())
     ]
 };
