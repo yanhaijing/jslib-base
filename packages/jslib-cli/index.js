8f463695bd06804820b8baa29f619eca3e563589
houce <houce@zhihu.com>
2019-09-06 19:20:19 +0800

    feat: add

--- packages/jslib-cli/index.js
+++ packages/jslib-cli/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
+var copydir = require('copy-dir')
+
 
 console.log(process.argv.slice(2))
+
+
+console.log(__dirname)
+
+ 
+// copydir.sync(`${__dirname}/base`, process.cwd(), {
+//   mode: true,    // keep file mode
+//   cover: true    // cover file when exists, default is true
+// })
