aac5c418115b65692481f977ca2963581f005409
yanhaijing <yanhaijing@yeah.net>
2019-09-07 18:25:24 +0800

    feat: update

---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+var path = require('path');
+var yargs = require('yargs');
+
+var cdkit = require('../index.js');
+
+yargs
+    .usage('usage: jslib [options]')
+    .usage('usage: jslib <command> [options]')
+    .example('jslib new myproject', '新建一个项目myproject')
+    .alias("h", "help")
+    .alias("v", "version")
+    .command(['new', 'n'], '新建一个项目', function (yargs) {
+        return yargs.option('type', {
+            alias: 'T',
+            default: 'js',
+            describe: '请选择项目的语言[js|ts]',
+            choices: ['js', 'ts'],
+        }).option('lang', {
+            alias: 'L',
+            default: 'zh',
+            describe: '请选择项目的语言[zh|en]',
+            choices: ['zh', 'en'],
+        })
+    }, function (argv) {
+        run('new', argv)
+    })
+    .command(['update', 'u'], '更新一个项目', function (yargs) {
+        run('update', yargs.argv)
+    })
+    .demandCommand()
+    .epilog('copyright 2018-2019')
+    .argv;
+
+function run(cmd, argv) {
+    // 运行命令
+    cdkit.run(cmd, argv);
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var path = require('path');
+
+function run(cmd, argv) {
+    console.log(cmd, argv)
+}
+
+exports.run = run;
