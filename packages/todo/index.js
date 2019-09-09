a6d07780f6217635c0bae9677d866cd3b154fd52
yanhaijing <yanhaijing@yeah.net>
2019-09-09 17:20:47 +0800

    feat: todo完成中英文功能

--- packages/todo/index.js
+++ packages/todo/index.js
@@ -2,11 +2,13 @@ const path = require('path');
 const util = require('@js-lib/util');
 
 function init(cmdPath, name, option) {
-    util.copyFile(path.resolve(__dirname, './template/TODO.md'), path.resolve(cmdPath, name, 'TODO.md'));
+    console.log('@js-lib/todo: init');
+    const lang = option.lang;
+    util.copyFile(path.resolve(__dirname, `./template/TODO.${lang}.md`), path.resolve(cmdPath, name, 'TODO.md'));
 }
 
 function update() {
-    console.log('@js-lib/todo: update')
+    console.log('@js-lib/todo: update');
 }
 
 module.exports = {
