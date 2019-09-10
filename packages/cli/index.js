383a3f08654cb0610b13d5f606b55e2e679272ef
yanhaijing <yanhaijing@yeah.net>
2019-09-10 11:05:59 +0800

    fix: 修复初始化的bug

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+
+const changelog = require('@js-lib/changelog');
+const doc = require('@js-lib/doc');
+const readme = require('@js-lib/readme');
 const todo = require('@js-lib/todo');
 
 function init(cmdPath, name, option) {
+    changelog.init(cmdPath, name, option)
+    doc.init(cmdPath, name, option)
+    readme.init(cmdPath, name, option)
     todo.init(cmdPath, name, option)
 }
 function update(cmdPath, option) {
