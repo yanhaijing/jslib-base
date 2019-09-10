c8646b028d5fbdc5aa6ca81036b4310c82cd8eb9
yanhaijing <yanhaijing@yeah.net>
2019-09-10 16:34:18 +0800

    feat: update root

--- packages/doc/index.js
+++ packages/doc/index.js
@@ -4,7 +4,7 @@ const util = require('@js-lib/util');
 function init(cmdPath, name, option) {
     console.log('@js-lib/doc: init');
     const lang = option.lang;
-    util.copyFile(path.resolve(__dirname, `./template/doc.${lang}.md`), path.resolve(cmdPath, name, 'doc.md'));
+    util.copyFile(path.resolve(__dirname, `./template/doc.${lang}.md`), path.resolve(cmdPath, name, './doc/doc.md'));
 }
 
 function update() {
--- packages/root/index.js
+++ packages/root/index.js
@@ -3,7 +3,10 @@ const util = require('@js-lib/util');
 
 function init(cmdPath, name, option) {
     console.log('@js-lib/root: init');
-    util.copyDir(path.resolve(__dirname, `./template`), path.resolve(cmdPath, name));
+    const lang = option.lang;
+    util.copyDir(path.resolve(__dirname, `./template/base`), path.resolve(cmdPath, name));
+    console.log(path.resolve(__dirname, `./template/ISSUE_TEMPLATE.${lang}.md`), path.resolve(cmdPath, name, './.github/ISSUE_TEMPLATE.md'))
+    util.copyFile(path.resolve(__dirname, `./template/ISSUE_TEMPLATE.${lang}.md`), path.resolve(cmdPath, name, './.github/ISSUE_TEMPLATE.md'));
 }
 
 function update() {
