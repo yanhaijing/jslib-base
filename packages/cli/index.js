4ffbba0acc108a0251d10ea90d6fa6bddcd6c328
yanhaijing <yanhaijing@yeah.net>
2019-09-16 18:09:02 +0800

    feat: 合并简单包

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -2,13 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const root = require('@js-lib/root');
-const changelog = require('@js-lib/changelog');
-const doc = require('@js-lib/doc');
 const eslint = require('@js-lib/eslint');
 const license = require('@js-lib/license');
 const package = require('@js-lib/package');
 const readme = require('@js-lib/readme');
-const todo = require('@js-lib/todo');
 const src = require('@js-lib/src');
 const demo = require('@js-lib/demo');
 const rollup = require('@js-lib/rollup');
@@ -16,13 +13,10 @@ const compiler = require('@js-lib/compiler');
 
 function init(cmdPath, option) {
     root.init(cmdPath, option.pathname, option);
-    changelog.init(cmdPath, option.pathname, option);
-    doc.init(cmdPath, option.pathname, option);
     eslint.init(cmdPath, option.pathname, option);
     license.init(cmdPath, option.pathname, option);
     package.init(cmdPath, option.pathname, option);
     readme.init(cmdPath, option.pathname, option);
-    todo.init(cmdPath, option.pathname, option);
     src.init(cmdPath, option.pathname, option);
     demo.init(cmdPath, option.pathname, option);
     rollup.init(cmdPath, option.pathname, option);
--- packages/root/index.js
+++ packages/root/index.js
@@ -5,7 +5,22 @@ function init(cmdPath, name, option) {
     console.log('@js-lib/root: init');
     const lang = option.lang;
     util.copyDir(path.resolve(__dirname, `./template/base`), path.resolve(cmdPath, name));
-    util.copyFile(path.resolve(__dirname, `./template/ISSUE_TEMPLATE.${lang}.md`), path.resolve(cmdPath, name, './.github/ISSUE_TEMPLATE.md'));
+    util.copyFile(
+        path.resolve(__dirname, `./template/ISSUE_TEMPLATE.${lang}.md`),
+        path.resolve(cmdPath, name, './.github/ISSUE_TEMPLATE.md')
+    );
+    util.copyFile(
+        path.resolve(__dirname, `./template/TODO.${lang}.md`),
+        path.resolve(cmdPath, name, './TODO.md')
+    );
+    util.copyFile(
+        path.resolve(__dirname, `./template/CHANGELOG.${lang}.md`),
+        path.resolve(cmdPath, name, './CHANGELOG.md')
+    );
+    util.copyFile(
+        path.resolve(__dirname, `./template/doc.${lang}.md`),
+        path.resolve(cmdPath, name, './doc/api.md')
+    );
 }
 
 function update() {
