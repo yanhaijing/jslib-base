494c1510c424d0fb3805339ad42a0eb6120fd20b
yanhaijing <yanhaijing@yeah.net>
2019-09-16 18:27:55 +0800

    feat: complete demo

--- packages/demo/index.js
+++ packages/demo/index.js
@@ -4,10 +4,14 @@ const util = require('@js-lib/util');
 function init(cmdPath, name, option) {
     console.log('@js-lib/demo: init');
     const lang = option.lang;
-    util.copyFile(path.resolve(__dirname, `./template/demo-amd.html`), path.resolve(cmdPath, name, 'demo/demo-amd.html'));
-    util.copyFile(path.resolve(__dirname, `./template/demo-global.html`), path.resolve(cmdPath, name, 'demo/demo-global.html'));
-    util.copyFile(path.resolve(__dirname, `./template/demo-node.js`), path.resolve(cmdPath, name, 'demo/demo-node.js'));
     util.copyFile(path.resolve(__dirname, `./template/require.js`), path.resolve(cmdPath, name, 'demo/js/require.js'));
+    util.copyFile(path.resolve(__dirname, `./template/demo-amd.html`), path.resolve(cmdPath, name, 'demo/demo-amd.html'));
+    util.copyFile(path.resolve(__dirname, `./template/demo-node.js`), path.resolve(cmdPath, name, 'demo/demo-node.js'));
+    util.copyTmpl(
+        path.resolve(__dirname, `./template/demo-global.html.tmpl`),
+        path.resolve(cmdPath, name, 'demo/demo-global.html'),
+        option,
+    );
 }
 
 function update() {
