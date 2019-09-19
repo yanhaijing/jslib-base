359ed372f879479bc27981e84ac6b65d9a1b3b28
houce <houce@zhihu.com>
2019-09-19 10:56:51 +0800

    feat: clean up and overwrite console functions with chalk.js

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -13,8 +13,6 @@ const demo = require('@js-lib/demo');
 const rollup = require('@js-lib/rollup');
 const test = require('@js-lib/test');
 
-const {log} = require('@js-lib/util');
-
 function init(cmdPath, option) {
     spinner.start('Starting project');
     
