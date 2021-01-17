76971bd365d509e716009904efd6cf86efe2229e
zenquan <1847867150@qq.com>
2021-01-17 18:01:28 +0800

    feat: 自动化生成文档支持ts文件

--- packages/doc/index.js
+++ packages/doc/index.js
@@ -1,20 +1,32 @@
 const path = require('path');
 const util = require('@js-lib/util');
 
-function init(cmdPath, name) {
+function init(cmdPath, name, option) {
     console.log('@js-lib/doc: init');
-
+    const type = option.type;
+    if (type === 'ts') {
+        util.copyFile(
+            path.resolve(__dirname, `./template/jsdoc2md.json`),
+            path.resolve(cmdPath, name, 'jsdoc2md.json')
+        );
+    }
     util.mergeJSON2JSON(
-        path.resolve(__dirname, `./template/package.json`),
+        path.resolve(__dirname, `./template/package.${type}.json`),
         path.resolve(cmdPath, name, './package.json')
     );
 }
 
-function update(cmdPath) {
+function update(cmdPath, option) {
     console.log('@js-lib/doc: update');
-
+    const type = option.type;
+    if (type === 'ts') {
+        util.copyFile(
+            path.resolve(__dirname, `./template/jsdoc2md.json`),
+            path.resolve(cmdPath, 'jsdoc2md.json')
+        );
+    }
     util.mergeJSON2JSON(
-        path.resolve(__dirname, `./template/package.json`),
+        path.resolve(__dirname, `./template/package.${type}.json`),
         path.resolve(cmdPath, './package.json')
     );
 }
