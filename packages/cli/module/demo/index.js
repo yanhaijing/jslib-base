6de533761806ed303df0b132a13f460193e68d9e
yanhaijing <yanhaijing@yeah.net>
2025-01-01 20:36:03 +0800

    feat: add ci

--- packages/cli/module/demo/index.js
+++ packages/cli/module/demo/index.js
@@ -6,20 +6,20 @@ function init(cmdPath, name, option) {
   const _lang = option.lang;
   util.copyFile(
     path.resolve(__dirname, `./template/require.js`),
-    path.resolve(cmdPath, name, 'demo/js/require.js')
+    path.resolve(cmdPath, name, 'demo/js/require.js'),
   );
   util.copyFile(
     path.resolve(__dirname, `./template/demo-amd.html`),
-    path.resolve(cmdPath, name, 'demo/demo-amd.html')
+    path.resolve(cmdPath, name, 'demo/demo-amd.html'),
   );
   util.copyFile(
     path.resolve(__dirname, `./template/demo-node.js`),
-    path.resolve(cmdPath, name, 'demo/demo-node.js')
+    path.resolve(cmdPath, name, 'demo/demo-node.js'),
   );
   util.copyTmpl(
     path.resolve(__dirname, `./template/demo-global.html.tmpl`),
     path.resolve(cmdPath, name, 'demo/demo-global.html'),
-    option
+    option,
   );
 }
 
